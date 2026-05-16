a6a8cb27a312079a21dcada3690d4096fbac58da
shivaprabha2997 <shivaprabha2997@users.noreply.github.com>
2026-05-16 15:37:43 +0530

    feat: implement devops project with pinned providers

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+  name = "tes-vpc"
+  cidr = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+  cluster_name    = "mycluster1"
+  cluster_version = "1.27"
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  eks_managed_node_groups = {
+    main = { desired_size = 3, min_size = 1, max_size = 5, instance_types = ["t3.medium"] }
+  }
+}
